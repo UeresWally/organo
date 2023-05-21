31c10983562318f9290419dab29ebe0eb48555df
Wallace <wallacesantoscs@gmail.com>
2023-05-21 10:25:04 -0300

    aula 03 curso de react da alura, aprendendo controladores, useState

--- src/componentes/CampoTexto/index.js
+++ src/componentes/CampoTexto/index.js
@@ -2,10 +2,17 @@ import './CampoTexto.css'
 
 const CampoTexto = (props) => {
     
+
+    //let valor = 'Wallace Caetano'
+
+    const aoDigitado = (evento) => {
+        props.aoAlterado(evento.target.value)
+    }
+
     return (
         <div className="campo-texto">
             <label>{props.label}</label>
-            <input required={props.obrigatorio} placeholder={props.placeholder}/>
+            <input value={props.valor} onChange={aoDigitado} required={props.obrigatorio} placeholder={props.placeholder}/>
         </div>
     )
 }
